992e6d2f941ac0bc3cad23469a91ade36d2f1c04
wojtko <j.w2001@onet.pl>
2024-02-01 23:07:04 +0100

    Minor changes to the Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Image
+        src="https://logos-world.net/wp-content/uploads/2021/02/Trello-Emblem.png"
+        width={300}
+        height={100}
+        alt="trello logo"
+      />
+    </header>
+  );
 }
 
 export default Header;
