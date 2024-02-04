d4ea88d4a2c29a4d88dd419afab62593e894b0c5
wojtko <j.w2001@onet.pl>
2024-02-04 22:40:56 +0100

    Change Header and Layout styles.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import React from "react";
 import Avatar from "react-avatar";
@@ -9,6 +9,24 @@ function Header() {
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10">
+        <div
+          className="
+        absolute
+        top-0
+        left-0
+        w-full
+        h-96
+        bg-gradient-to-br
+        from-pink-400
+        to-[#0055d1]
+        rounded-md
+        filter
+        blur-3xl
+        opacity-50
+        -z-50
+        "
+        />
+
         <Image
           src="https://1000logos.net/wp-content/uploads/2021/05/Trello-Logo-2011.png"
           width={300}
@@ -17,7 +35,7 @@ function Header() {
           className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
         />
 
-        <div className="">
+        <div className="flex items-center space-x-5 flex-1 justify-end w-full">
           <form
             className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1
         md:flex-initial"
@@ -34,8 +52,18 @@ function Header() {
             </button>
           </form>
 
-          {/* <Avatar name="Jakub Wojtkowski" /> */}
+          {/* <Avatar name="Jakub Wojtkowski" round size="50" color="0055d1" /> */}
         </div>
+      </div>
+
+      <div className="flex items-center justify-center px-5 py-2 md:py-5">
+        <p
+          className="flex items-center text-sm font-light bg-white pr-5 shadow-xl
+        rounded-xl w-fit italic max-w-3xl text-[#0055d1]"
+        >
+          <UserCircleIcon className="inline-block h-10 w-10 text-[#0055d1] mr-1" />
+          GPT is summarising your tasks for the day...
+        </p>
       </div>
     </header>
   );
