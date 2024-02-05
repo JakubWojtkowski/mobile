16db9e5fce837becea845b274e4917497bba95fa
wojtko <j.w2001@onet.pl>
2024-02-05 22:51:43 +0100

    Configure Appwrite. Create Board component

---
+++ components/Board.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+
+function Board() {
+  return (
+    <DragDropContext>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => {
+          <div>{/* rendering all the columns */}</div>;
+        }}
+      </Droppable>
+    </DragDropContext>
+  );
+}
+
+export default Board;
