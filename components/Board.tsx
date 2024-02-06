a93dc9b24698042eeb45378361844649353f8257
wojtko <j.w2001@onet.pl>
2024-02-06 18:51:13 +0100

    Fetch data from Appwrite cloud. Building the Board Component. Implementing todo data. Sorting columns.

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,14 +1,35 @@
-import React from "react";
+"use client";
+
+import { useBoardStore } from "@/store/BoardStore";
+import React, { useEffect } from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 function Board() {
+  const [board, getBoard] = useBoardStore((state) => [
+    state.board,
+    state.getBoard,
+  ]);
+
+  useEffect(() => {
+    getBoard();
+  }, [getBoard]);
+
   return (
     <DragDropContext>
       <Droppable droppableId="board" direction="horizontal" type="column">
-        {(provided) => {
-          <div>{/* rendering all the columns */}</div>;
-        }}
+        {(provided) => (
+          <div
+            className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto"
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {Array.from(board.columns.entries()).map(([id, column], index) => (
+              <Column key={id} id={id} todos={column.todos} index={index} />
+            ))}
+          </div>
+        )}
       </Droppable>
+      )
     </DragDropContext>
   );
 }
