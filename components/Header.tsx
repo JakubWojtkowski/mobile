9433a2344e75a151bf6ac885cde0c5351b0cbf55
wojtko <j.w2001@onet.pl>
2024-02-09 14:22:36 +0100

    Configure openai. Fetch suggestion on the frontend. Create fetchSuggestion func

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,31 @@
 "use client";
 
+import fetchSuggestion from "@/lib/fetchSuggestion";
 import { useBoardStore } from "@/store/BoardStore";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
-import React from "react";
+import { useEffect, useState } from "react";
 import Avatar from "react-avatar";
 
 function Header() {
-  const [searchString, setSearchString] = useBoardStore((state) => [
+  const [board, searchString, setSearchString] = useBoardStore((state) => [
+    state.board,
     state.searchString,
     state.setSearchString,
   ]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [suggestion, setSuggestion] = useState<string>("");
+
+  useEffect(() => {
+    if (board.columns.size === 0) return;
+    setLoading(true);
+
+    const fetchSuggestionFunc = async () => {
+      const suggestion = await fetchSuggestion(board);
+      setSuggestion(suggestion);
+      setLoading(false);
+    };
+  }, [board]);
 
   return (
     <header>
